80247ba111d5410a22b938b6ad9a38a1e1c2fbaf
Dan Davison <dan.davison@temporal.io>
2024-01-14 21:22:08 -0500

    Render: group history events into WFTs

--- manim_renderer/history.py
+++ manim_renderer/history.py
@@ -4,7 +4,7 @@ from manim import DOWN
 from manim import GREEN_D as GREEN
 from manim import LEFT
 from manim import RED_D as RED
-from manim import SMALL_BUFF, Mobject, VGroup
+from manim import SMALL_BUFF, Mobject, SurroundingRectangle, VGroup
 
 from manim_renderer import style
 from manim_renderer.entity import ProxyEntity, ProxyEntityWithChildren, VisualElement
@@ -34,9 +34,44 @@ class History(
     child_cls = HistoryEvent
     child_align_direction = LEFT
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.history_event_group_mobjs: list[Mobject] = []
+
     @staticmethod
     def render(_: schema.History) -> Mobject:
         return style.invisible_point()
+
+    def render_to_scene(self, entity: schema.History):
+        super().render_to_scene(entity)
+        self.history_event_groups_render_to_scene(entity)
+
+    def history_event_groups_render_to_scene(self, entity: schema.History):
+        child_entities = self.get_child_entities(entity)
+        assert len(self.children) == len(child_entities)
+        wfts: list[list[Mobject]] = []
+        wft: list[Mobject] = []
+        for c, e in zip(self.children, child_entities):
+            if e.event_type == schema.HistoryEventType.WFT_SCHEDULED:
+                assert not wft
+                wft.append(c.mobj)
+            elif e.event_type == schema.HistoryEventType.WFT_COMPLETED:
+                wft.append(c.mobj)
+                wfts.append(wft)
+                wft = []
+            elif wft:
+                wft.append(c.mobj)
+        self.scene.remove(*self.history_event_group_mobjs)
+        self.history_event_group_mobjs = [
+            SurroundingRectangle(
+                VGroup(*wft),
+                color=style.COLOR_HISTORY_EVENT_GROUP_RECT,
+                stroke_width=style.STROKE_WIDTH_HISTORY_EVENT_GROUP_RECT,
+                buff=0.05,
+            )
+            for wft in wfts
+        ]
+        self.scene.add(*self.history_event_group_mobjs)
 
     @staticmethod
     def get_child_entities(entity: schema.History) -> list[schema.HistoryEvent]:
